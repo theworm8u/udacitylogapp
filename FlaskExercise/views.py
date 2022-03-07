e1fbc7b7b306508920ba9dcd3df8fdd5bb6ac5c7
David Whitehead <david.k.whitehead@gmail.com>
2022-03-06 22:09:30 -0500

    added condition for errors

--- FlaskExercise/views.py
+++ FlaskExercise/views.py
@@ -7,6 +7,16 @@ def home():
     log = request.values.get('log_button')
     # TODO: Appropriately log the different button presses
     #   with the appropriate log level.
+    if log:
+        if log == 'info':
+            app.logger.info('No issue.')
+        elif log == 'warning':
+            app.logger.warning('Warning occurred.')
+        elif log == 'error':
+            app.logger.error('Error occurred.')
+        elif log == 'critical':
+            app.logger.critical('Critical error occurred.')
+        
     return render_template(
         'index.html',
         log=log
